bb80e1a4f0977b5b84bdf1bfd2433b84bad9bed3
Fernanda Hiarita <159085016+fehiarita@users.noreply.github.com>
2025-08-07 18:42:16 -0300

    aprendendo variáveis de ambiente

--- Projetos/Projeto 02/ProfileConfig/src/main/java/com/DIO/ProfileConfig/AppController.java
+++ Projetos/Projeto 02/ProfileConfig/src/main/java/com/DIO/ProfileConfig/AppController.java
@@ -10,10 +10,18 @@ public class AppController {
     @Value("${app.message}")
     private String appMessage ;
 
+    @Value("${ENV_DB_URL:NENHUMA}")
+    private String dbVariavelAmbiente;
+
     //endpoint GET
     //exibe mensagem vindo do application.properties
     @GetMapping("/")
     public String getAppMessage() {
         return appMessage;
+    }
+
+    @GetMapping("/env")
+    public String getDbVariavelAmbiente() {
+        return "A seguinte variável foi passada: " + dbVariavelAmbiente;
     }
 }
